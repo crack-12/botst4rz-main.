738cbc138a422e9aeef60720f4755599a385938e
crack-12 <80333387+crack-12@users.noreply.github.com>
2021-04-07 00:23:04 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,121 @@
+
+export.menu = (id, BotName, corohelp, showDate, showTime, instagramlu, whatsapplu, cuando está activo, grupch1, grupch2) => {
+ return `🔰 ----- [* MENU $ {BotName} *] ----- 🔰
+  
+ Hola, * $ {id.split ("@ s.whatsapp.net") [0]} * 👋️
+ ¡Estas son algunas de las características de este bot! ✨
+
+ ⚠️ * $ {showDate} *
+ ⚠️ * $ {ShowTime} *
+
+ ♻ Comando / Comando:
+   
+ => * 1. FABRICANTE DE PEGATINAS *
+ _ $ {BotName} le enviará su calcomanía con la leyenda #sticker! _
+ Comando: #sticker
+ Ejemplo: envíe una imagen con el título #sticker
+
+ => * 2. NÚMEROS AUTOMÁTICOS *
+ _ $ {BotName} enviará el texto del libro a medida que lo envíe.
+ Comando: #escribir [palabra]
+ Ejemplo: #escribe una estrella hermosa
+
+ => * 3.CONECTIVAS CARACTERÍSTICAS *
+ _ $ {BotName} enviará una rima aleatoria. _
+ Comando: #pantun
+ Ejemplo: #pantun
+
+ => * 4. DESCARGADOR DE YOUTUBE *
+ _ $ {BotName} enviará un descargador de videos desde yt de acuerdo con sus pedidos. _
+ Comando: #yt [enlace]
+ Ejemplo: #yt https://youtu.be/blablabla
+
+ => * 5. CARACTERÍSTICAS DE LAS COTIZACIONES *
+ _ $ {BotName} enviará una palabra de sabiduría al azar. _
+ Comando: #quotes
+ Ejemplo: #quotes
+
+ => * 6.Línea de tiempo estándar *
+ _ $ {BotName} enviará la imagen cogan / cecan de acuerdo con lo que envíes. _
+ Comando: #ptl [niña / niño]
+ Ejemplo: #ptl girl
+
+ => * 7. CARACTERÍSTICAS DEL ANIME *
+ _ $ {BotName} te enviará imágenes de anime al azar. _
+ Comando: #randomanime
+ Ejemplo: #randomanime
+
+ => * 8. CAMBIADOR DE VOZ DE GOOGLE *
+ _ $ {BotName} enviará la voz vn desde google de acuerdo con el comando que enviaste. _
+ Comando: #ttsid [palabra]
+ Ejemplo: #ttsid estrella hermosa
+
+ => * 9.CURAN CARACTERÍSTICAS *
+ _ $ {BotName} enviará versículos del Corán al azar. _
+ Comando: #quran
+ Ejemplo: #quran
+
+ => * 10. INFORMACIÓN DEL BOT *
+ _ $ {BotName} enviará información sobre el bot. _
+ Comando: #info
+ Ejemplo: #info
+
+ => * 11. DIGA CARACTERÍSTICAS *
+ _ $ {BotName} enviará las palabras de acuerdo con lo que envíes. _
+ Comando: #decir [palabra]
+ Ejemplo: #say hola guapo estrella
+
+ => * 12.DESCARGADOR MP3 YOUTUBE *
+ _ $ {BotName} enviará la canción de acuerdo con el comando que enviaste. _
+ Comando: # ytmp3 [enlace]
+ Ejemplo: # ytmp3 https://youtu.be/xxxx
+
+ => * 13.DESCARGADOR DE VIDEO / FOTO INSTAGRAM *
+ _ $ {BotName} enviará video / foto de ig de acuerdo con el pedido que enviaste. _
+ Comando: #ig [enlace]
+ Ejemplo: #ig https://instagram.com/xxxx
+
+ => * 14.FACEBOOK VIDEO / FOTO DOWNLOADER *
+ _ $ {BotName} enviará el video / foto de Facebook de acuerdo con el pedido que enviaste. _
+ Comando: #fb [enlace]
+ Ejemplo: #fb https://facebook.com/xxxx
+
+ => * 15.DESCARGADOR DE VIDEO / FOTO DE TWITTER *
+ _ $ {BotName} enviará un video / foto de Twitter de acuerdo con el comando que enviaste. _
+ Comando: #twt [enlace]
+ Ejemplo: #twt https://twitter.com/xxxx
+
+ => * 16.WIKIPEDIA *
+ _ $ {BotName} enviará el resultado de wikipedia de acuerdo con el comando que enviaste. _
+ Comando: #wiki [consulta]
+ Ejemplo: #wiki dog
+
+ ♻️ ¡NO OLVIDE DONAR PARA CONTINUAR LOS BOTS ACTIVOS!
+ ♻️ ¿QUIERES DONAR?  POR FAVOR ESCRIBA # donar
+
+ 📺 * Publicidad *:
+
+ ✅ Siga la cuenta de instagram admin $ {instagramlu}
+
+ ⚠️ ¡ÚLTIMA INFORMACIÓN SOBRE COVID-19!
+
+ ⚠️ POSITIVO: * $ {corohelp.confirmed.value} *
+ ⚠️ SANAR: * $ {corohelp.recovered.value} *
+ ⚠️ MUERTO: * $ {corohelp.deaths.value} *
+ ⚠️ ACTUALIZACIÓN: * $ {corohelp.lastUpdate} *
+
+ ♻️ _ ¡MANTÉNGASE SANO Y UTILICE SIEMPRE UNA MASCARILLA! _
+
+ ♻️ ¿Quieres anunciarte en * $ {BotName}? *
+ ☎️ WA: * $ {whatsapplu} *
+  
+ ⚠️ Úselo sabiamente‼ ️
+ ⚠️ Este bot ejecuta $ {cuando está activo el bot}‼ ️
+
+ ✳️ Grub oficial [1]: $ {el crack.r.informatico}
+
+ ✳️ Grub oficial [1]: $ {7}
+
+  
+ 🔰 ----- [* POTENCIADO POR $ {BotName} *] ----- el crack.r.informatico 🔰`
+ }
